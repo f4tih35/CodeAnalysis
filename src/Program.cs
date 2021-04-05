c69a87eb6c144a4b96555c48929023af2ce079e8
Fatih Melih Ersoy <fatihmelih9@hotmail.com>
2021-04-05 16:03:42 +0300

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+
+namespace CodeAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var text = System.IO.File.ReadAllText(@"Code.txt");
+            Console.WriteLine(text);
+            var tree = CSharpSyntaxTree.ParseText(text);
+            var root = tree.GetRoot();
+
+            var directives = root.DescendantNodes().OfType<UsingDirectiveSyntax>().ToList();
+
+            foreach (var item in directives)
+            {
+                Console.WriteLine(item);
+            }   
+        }
+    }
+}
